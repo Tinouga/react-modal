e2fc2352ce3f204c1882b6ceccb25dd3c5220589
Angel <angeldevvvv@gmail.com>
2024-11-04 09:24:10 +0100

    fixed error in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {resolve} from 'path';
+import {resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
